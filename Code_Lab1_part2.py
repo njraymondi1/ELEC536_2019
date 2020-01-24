541794d2e714ff760bee5d4712cdb66b6f8267eb
Nate Raymondi <njraymondi1@gmail.com>
2020-01-24 16:40:17 -0600

    Add files via upload
    Lab1

---
+++ Code_Lab1_part2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import SoapySDR
+print('\n'.join(SoapySDR.listModules()))
+print(SoapySDR.getRootPath())
+print(SoapySDR.getAPIVersion())
